9acfa9f0d4ec5c690f2ae6e8d2a6ef23d9098593
valerio <corallini.valerio.1999@gmail.com>
2023-02-07 18:01:39 +0100

    third exercise - numbers

---
+++ src/it/develhope/exercise_four_numbers.java
@@ -0,0 +1,125 @@
+package it.develhope;
+
+public class exercise_four_numbers {
+    // Don't forget to use comments to clear things up if you need them!
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4Bonus();
+        exercise5Bonus();
+        playAroundWithModulus();
+    }
+
+    /*
+            Remember the symbols you need to do maths are
+
+            Plus = +
+            Minus = -
+            Multiply or times = *
+            Divide = /
+     */
+
+    /**
+     * 1: Create an int variable, add 1 to the value and then print it. Use the + operator
+     * Then minus 1 from the same variable and print it.
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        int number = 1;
+        System.out.println("number - 1: " + (number - 1));
+        System.out.println("number + 1: " + (number + 1));
+    }
+
+    /**
+     * 2: Create a double variable, times by 2 and then print it.
+     * <p>
+     * Create another double variable, which is the previous variable divided by 2
+     * and then print it
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        double dub = 5.45;
+        System.out.println("Doubled variable is: " + (dub * 2));
+        System.out.println("Divided variable is: " + (dub / 2));
+    }
+
+    /**
+     * 3: We are going to convert the temperature from celsius to fahrenheit
+     * <p>
+     * Create a double variable called celsius
+     * <p>
+     * Create a new variable called fahrenheit
+     * <p>
+     * The variable fahrenheit should be set as
+     * celsius times by 1.8 plus 32
+     * <p>
+     * Print out celsius and fahrenheit with 2 print statements
+     */
+    private static void exercise3() {
+        double celsius = 28.0;
+        double fahrenheit = (celsius*1.8) + 32;
+        // Write your code here
+        System.out.println("\nExercise 3:");
+        System.out.println("temperature in celsius: " + celsius + "°C");
+        System.out.println("temperature in fahrenheit: " + fahrenheit + "°F");
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age with a starting value of 10.
+     * <p>
+     * Only using the '++' operator increase this value to 15
+     */
+    private static void exercise4Bonus() {
+        System.out.println("\nExercise 4:");
+        int age = 10;
+        System.out.println("age: " + age);
+        for (int i = age; i < 15; i++){
+            age++;
+        }
+        System.out.println("age incremented: " + age);
+    }
+
+    /**
+     * 5: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age set as 50
+     * <p>
+     * Using the -= operator change this value from 50 to 0 in one line of code,
+     * <p>
+     * Underneath create another int called secondAge set as 50
+     * <p>
+     * Change this value from 50 to 0 in 3 lines of code
+     */
+    private static void exercise5Bonus() {
+        System.out.println("\nExercise 5:");
+
+        int age = 50;
+        System.out.println("age: " + age);
+        age -= age;
+        System.out.println("age decremented in one line: " + age);
+
+        int secondAge = 50;
+        while (secondAge > 0){
+            secondAge--;
+        }
+        System.out.println("age decremented in three lines: " + secondAge);
+    }
+
+    /**
+     * This is completely optional, but if you guys want to play around with
+     * modulus to get a better understanding. Simply change the code below
+     */
+    private static void playAroundWithModulus() {
+        System.out.println("\nModulus testing!:");
+
+        int value = 5;   // <--- change this value
+        int modulus = 4; // <--- and this value to see the results of modulus
+
+        System.out.println("Modulus calculation, " + value + " % " + modulus +
+                "=" + value % modulus);
+    }
+
+}
