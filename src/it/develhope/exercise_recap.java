78cbd610438354fbac6954cab6299ef4f4b6c5ee
valerio <corallini.valerio.1999@gmail.com>
2023-02-09 20:25:34 +0100

    Eleventh  exercise - recap

--- src/it/develhope/exercise_recap.java
+++ src/it/develhope/exercise_recap.java
@@ -15,27 +15,40 @@ public class exercise_recap {
     
     /**
      * 1: We are going to 'score' strings, given a string you are to calculate its score based on the following factors.
-     *
-     *    Start with a base score of the string's length, this is the starting score. Use .length() on a string
-     *
-     *    The Maximum starting score is 20, if a score is over 20. Set it to 20.
-     *
-     *    If the string contains the letter a, -5 from the score
-     *
-     *    If the string is all lowercase, add 10 to the score.
-     *
-     *    If the score is now over 20, times it by 2
+     * <p>
+     * Start with a base score of the string's length, this is the starting score. Use .length() on a string
+     * <p>
+     * The Maximum starting score is 20, if a score is over 20. Set it to 20.
+     * <p>
+     * If the string contains the letter a, -5 from the score
+     * <p>
+     * If the string is all lowercase, add 10 to the score.
+     * <p>
+     * If the score is now over 20, times it by 2
      */
     public static double exercise1(String text, double expectedScore) {
         double yourScore = 0;
         
         // Start your code here
+        yourScore = text.length();
         
+        if (text.length() > 20) {
+            yourScore = text.length() - (text.length() - 20);
+        }
+        if (text.contains("a")) {
+            yourScore = yourScore - 5;
+        }
+        if (text.equals(text.toLowerCase())) {
+            yourScore = yourScore + 10;
+        }
+        if (yourScore > 20) {
+            yourScore = yourScore * 2;
+        }
         // End it here
         
-        System.out.print("Expected score=" + expectedScore +", actual score=" + yourScore);
+        System.out.print("Expected score=" + expectedScore + ", actual score=" + yourScore);
         if (expectedScore != yourScore) {
-            System.out.println(" - Failed: Wrong score for '" + text +"'");
+            System.out.println(" - Failed: Wrong score for '" + text + "'");
         } else {
             System.out.println(" - Success!");
         }
@@ -45,22 +58,30 @@ public class exercise_recap {
     
     /**
      * 2: We are going to 'score' a meal, given a string of mealType you are to calculate its score based on the following factors.
-     *
-     *    For the meal type sandwich calculate score like the following: 5 * weight
-     *
-     *    For the meal type of soup calculate score like the following 3 * weight
-     *
-     *    The minimum score should be 5, if a score is lower than this increase it to 5
+     * <p>
+     * For the meal type sandwich calculate score like the following: 5 * weight
+     * <p>
+     * For the meal type of soup calculate score like the following 3 * weight
+     * <p>
+     * The minimum score should be 5, if a score is lower than this increase it to 5
      */
     public static double exercise2(String mealType, double weight, double expectedScore) {
         double yourMealScore = 0;
         // Start your code here
-        
+        switch (mealType) {
+            case "sandwich" -> yourMealScore = 5 * weight;
+            case "soup" -> yourMealScore = 3 * weight;
+            default -> {
+            }
+        }
+        if (yourMealScore < 5){
+            yourMealScore = 5;
+        }
         // End it here
         
-        System.out.print("Expected score=" + expectedScore +", actual score=" + yourMealScore);
+        System.out.print("Expected score=" + expectedScore + ", actual score=" + yourMealScore);
         if (expectedScore != yourMealScore) {
-            System.out.println(" - Failed: Wrong score for '" + mealType +"'");
+            System.out.println(" - Failed: Wrong score for '" + mealType + "'");
         } else {
             System.out.println(" - Success!");
         }
