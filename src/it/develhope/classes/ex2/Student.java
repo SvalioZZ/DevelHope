44278ac0ec787b21325f0482633e1f580afad479
valerio <corallini.valerio.1999@gmail.com>
2023-02-19 19:21:02 +0100

    Exercise 18 - Encapsulation

---
+++ src/it/develhope/classes/ex2/Student.java
@@ -0,0 +1,28 @@
+package it.develhope.classes.ex2;
+
+public class Student {
+    String name;
+    int age;
+    
+    public Student(String name, int age) {
+        System.out.println("Constructing...");
+        System.out.println("Constructed!");
+        this.name = name;
+        this.age = age;
+    }
+    
+    public Student (String name){
+        this.name = name;
+    }
+    
+    public Student (int age){
+        this.age = age;
+    }
+    
+    public Student () { }
+    
+    @Override
+    public String toString() {
+        return "*** New Student ***\nName: " + name + "\nAge: " + age;
+    }
+}
