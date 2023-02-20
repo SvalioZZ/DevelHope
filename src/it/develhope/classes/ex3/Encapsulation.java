92b42420a9aafb80cd21a88e2de0051f1828f94e
valerio <corallini.valerio.1999@gmail.com>
2023-02-20 15:46:14 +0100

    Exercise 19 - Encapsulation

--- src/it/develhope/classes/ex3/Encapsulation.java
+++ src/it/develhope/classes/ex3/Encapsulation.java
@@ -1,2 +1,71 @@
-package it.develhope.classes.ex3;public class Encapsulation {
+package it.develhope.classes.ex3;
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
+    
+    /**
+     * 1: Copy over the student class
+     * <p>
+     * Make sure there are 5 fields inside, if they are not simply create some more.
+     * make every field private first.
+     * <p>
+     * Then create getters and setters for each of these 5 fields.
+     * <p>
+     * Once ready use all 5 below
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        Student student = new Student();
+        // Write and use your 5 getters and setters!
+        
+        student.setName("Valerio");
+        student.setAge(23);
+        student.setSurname("Corallini");
+        student.setAddress("Via Napoli");
+        student.setAddressId(11);
+        
+        System.out.println(
+                student.getName() + "\n" +
+                student.getAge() + "\n" +
+                student.getSurname() + "\n" +
+                student.getAddress() + "\n" +
+                student.getAddressId()
+        
+        );
+    }
+    
+    /**
+     * 2: Create 3 setter constraints for the student class
+     * <p>
+     * Think about what reasonably constraints are?
+     * <p>
+     * Should a student be able to be a negative age?
+     * <p>
+     * Create a counter for one of the getters, try to think about
+     * what other code could be inside a getter? Why not write it?
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        //Write your code here
+        Student student = new Student();
+        student.setName("Viorica Gabriela");
+        student.setAge(24);
+        student.setSurname("Hacman");
+        student.setAddress("Via Napoli");
+        student.setAddressId(11);
+    
+        System.out.println(
+                student.getName() + "\n" +
+                        student.getAge() + "\n" +
+                        student.getSurname() + "\n" +
+                        student.getAddress() + "\n" +
+                        student.getAddressId()
+    
+        );
+        
+    }
+    
 }
--- src/it/develhope/classes/ex3/Student.java
+++ src/it/develhope/classes/ex3/Student.java
@@ -1,2 +1,98 @@
-package it.develhope.classes.ex3;public class Student {
-}
+package it.develhope.classes.ex3;
+
+
+    public class Student {
+        private String name;
+        private int age;
+        private String surname;
+        private String address;
+        private int addressId;
+    
+        public String getName() {
+            return name;
+        }
+    
+        public Student setName(String name) {
+            char [] chars = name.toCharArray();
+            for (char c : chars) {
+                if (Character.isDigit(c)){
+                    return this;
+                }
+            }
+            this.name = name;
+            return this;
+        }
+    
+        public int getAge() {
+            return age;
+        }
+    
+        public Student setAge(int age) {
+            if (age > 0) {
+                this.age = age;
+            } else {
+                System.out.println("Invalid");
+            }
+            return this;
+        }
+    
+        public String getSurname() {
+            return surname;
+        }
+    
+        public Student setSurname(String surname) {
+            char [] chars = name.toCharArray();
+            for (char c : chars) {
+                if (Character.isDigit(c)){
+                    return this;
+                }
+            }
+            this.name = name;
+            this.surname = surname;
+            return this;
+        }
+    
+        public String getAddress() {
+            return address;
+        }
+    
+        public Student setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+    
+        public int getAddressId() {
+            return addressId;
+        }
+    
+        public Student setAddressId(int addressId) {
+            this.addressId = addressId;
+            return this;
+        }
+    
+        public Student(String name, int age) {
+            System.out.println("Constructing...");
+            System.out.println("Constructed!");
+            this.name = name;
+            this.age = age;
+        }
+    
+        public Student(String name) {
+            this.name = name;
+        }
+    
+        public Student(int age) {
+            this.age = age;
+        }
+    
+        public Student() {
+        }
+    
+        @Override
+        public String toString() {
+            return "*** New Student ***\nName: " + name + "\nAge: " + age;
+        }
+        
+        
+    }
+
