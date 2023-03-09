aaa7c6344239f20d23f3a54cceea12b161716230
Valerio <corallini.valerio.1999@gmail.com>
2023-03-09 13:30:36 +0100

    new folder and new business :)

---
+++ src/it/develhope/challenge/openAi/Main.java
@@ -0,0 +1,29 @@
+package it.develhope.challenge.openAi;
+
+/**
+ * Exercise: File Manipulation
+ * You are given a file called input.txt which contains a list of integers. Your task is to read the integers from the file, sort them in ascending order, and write them to a new file called output.txt.
+ *
+ * Write a Java program that performs the following steps:
+ *
+ * Read the integers from the input.txt file.
+ * Sort the integers in ascending order.
+ * Write the sorted integers to the output.txt file.
+ * Your program should be able to handle the case where input.txt contains non-integer values or is not found, and should output a useful error message in these cases.
+ *
+ * Here are some additional requirements for your program:
+ *
+ * You should use the Scanner class to read from the input.txt file.
+ * You should use the Arrays.sort() method to sort the integers.
+ * You should use the PrintWriter class to write to the output.txt file.
+ * You should use exception handling to handle any errors that may occur while reading or writing to the files.
+ * You can assume that the input file contains at most 10,000 integers, with each integer having a value between -10^6 and 10^6.
+ */
+
+public class Main {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+}
