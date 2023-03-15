b30a4a5e7cc051d3c2f0abfd4037a4773cabb440
Valerio <corallini.valerio.1999@gmail.com>
2023-03-15 19:19:19 +0100

    OpenAi project Student Management App

--- src/it/develhope/challenge/openAi/ex_2/Main.java
+++ src/it/develhope/challenge/openAi/ex_2/Main.java
@@ -1,2 +1,116 @@
-package it.develhope.challenge.openAi.ex_2;public class Main {
+package it.develhope.challenge.openAi.ex_2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        StudentManagement studentManagement = new StudentManagement();
+        Student std1 = new Student("John", "Doyle", 0);
+        Student std2 = new Student("Francis", "Capone", 1);
+        Student std3 = new Student("Martin", "Doyle", 2);
+        Student std4 = new Student("Jeremy", "Irons", 3);
+        Student std5 = new Student("Helena", "Bonham Carter", 4);
+        Student std6 = new Student("Jessica", "Alba", 5);
+        Student std7 = new Student("Dorothy", "Farrel", 6);
+        
+        studentManagement.addStudent(std1);
+        studentManagement.addStudent(std2);
+        studentManagement.addStudent(std3);
+        studentManagement.addStudent(std4);
+        studentManagement.addStudent(std5);
+        studentManagement.addStudent(std6);
+        studentManagement.addStudent(std7);
+        
+        printOptions();
+        Scanner scanner = new Scanner(System.in);
+        Scanner scannerString = new Scanner(System.in);
+        Scanner inputInt = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        
+        while (userInput != 0) {
+            switch (userInput) {
+                case 1:
+                    scannerString.nextLine();
+                    System.out.println("Insert the name of the new student:");
+                    Student newStudent = new Student();
+                    newStudent.setName(scannerString.nextLine());
+                    System.out.println("Insert the surname of the new student:");
+                    newStudent.setSurname(scannerString.nextLine());
+                    System.out.println("Insert the ID of the new student:");
+                    newStudent.setId(scannerString.nextInt());
+                    studentManagement.addStudent(newStudent);
+                    System.out.println("New student has been added!\n\n");
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 2:
+                    System.out.println("All students:");
+                    studentManagement.printStudents();
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 3:
+                    System.out.println("Select the ID of the student you want to delete:");
+                    studentManagement.removeStudent(studentManagement.getStudents(inputInt.nextInt()));
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 4:
+                    System.out.println("Select the ID of the student you want to search:");
+                    Student searched = studentManagement.getStudents(inputInt.nextInt());
+                    searched.printInfo();
+                    System.out.println("\n");
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 5:
+                    System.out.println("Ordered list of students by surname:\n");
+                    studentManagement.sortStudentsBySurname();
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 6:
+                    System.out.println("Reordered list of students by ID:\n");
+                    studentManagement.sortById();
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+                case 7:
+                    System.out.println("Exiting program...");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        System.out.println("There was an error!" + e.getMessage());
+                    }
+                    System.out.println("*** Correctly exited program ***");
+                    userInput = 0;
+                    break;
+                default:
+                    System.out.println("Insert a valid option!");
+                    printOptions();
+                    userInput = scanner.nextInt();
+                    break;
+            }
+        }
+    }
+    
+    public static void printOptions() {
+        System.out.println("""
+                Welcome to Student Management System!
+                
+                What do you want to do?
+                
+                1. Add a new student;
+                2. View all students;
+                3. Delete a student;
+                4. Search for a student by id;
+                5. Order a student by surname;
+                6. Reorder all students by ID;
+                7. Exit program.
+                
+                """);
+    }
+    
 }
--- src/it/develhope/challenge/openAi/ex_2/StudentManagement.java
+++ src/it/develhope/challenge/openAi/ex_2/StudentManagement.java
@@ -1,2 +1,86 @@
-package it.develhope.challenge.openAi.ex_2;public class StudentManagement {
+package it.develhope.challenge.openAi.ex_2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StudentManagement {
+    
+    private List<Student> students;
+    
+    public StudentManagement() {
+        this.students = new ArrayList<>();
+    }
+    
+    public void printStudents() {
+        for (Student student : students) {
+            System.out.println(student.getName() + " - " + student.getSurname() + " - " + student.getId());
+        }
+    }
+    
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+    
+    public boolean removeStudent(Student student) {
+        return students.remove(student);
+    }
+    
+    public Student getStudents(int id) {
+        Student result = new Student();
+        try {
+            result = students.get(id);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return result;
+    }
+    
+    public void sortById() {
+        int n = students.size();
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (students.get(j).getId() > students.get(j + 1).getId()){
+                    Student temp = students.get(j);
+                    students.set(j, students.get(j + 1));
+                    students.set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    
+    public void sortStudentsBySurname() {
+//      List<Student> sortedStudents = students.stream().sorted(Comparator.comparing(Student::getSurname)).toList();
+//      for (Student student : sortedStudents) {
+//          student.printInfo();
+//      }
+        int n = students.size();
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (students.get(j).getSurname().compareTo(students.get(j + 1).getSurname()) > 0) {
+                    Student temp = students.get(j);
+                    students.set(j, students.get(j + 1));
+                    students.set(j + 1, temp);
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped in the inner loop, the list is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+        students.forEach(Student::printInfo);
+    }
+    
 }
