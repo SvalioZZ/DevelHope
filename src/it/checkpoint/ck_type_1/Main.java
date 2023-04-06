dcde3e88dc98894b249b63c6d9dfc54f731ce619
Valerio <corallini.valerio.1999@gmail.com>
2023-04-06 16:23:49 +0200

    Checkpoint files load and map generating writed in file

--- src/it/checkpoint/ck_type_1/Main.java
+++ src/it/checkpoint/ck_type_1/Main.java
@@ -1,2 +1,19 @@
-package it.checkpoint.ck_type_1;public class Main {
+package it.checkpoint.ck_type_1;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+    
+        List<String> lines = Simulation.loadLines("src/it/checkpoint/ck_type_1/resources/test.txt");
+    
+        HashMap<String, Integer> map = Simulation.processLines(lines);
+        
+        Simulation.writeLines("src/it/checkpoint/ck_type_1/resources/result.txt", map);
+        
+    }
+    
 }
--- src/it/checkpoint/ck_type_1/Simulation.java
+++ src/it/checkpoint/ck_type_1/Simulation.java
@@ -1,2 +1,52 @@
-package it.checkpoint.ck_type_1;public class Simulation {
+package it.checkpoint.ck_type_1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Simulation {
+    
+    public static List<String> loadLines(String filePath) throws IOException {
+        /*FileReader fileReader = new FileReader(filePath);
+        BufferedReader br = new BufferedReader(fileReader);
+        List<String> lines = new ArrayList<String>();
+        while (br.ready()) {
+            lines.add(br.readLine());
+        }
+        br.close();
+        return lines;*/
+        // return Files.readAllLines(Path.of(filePath));
+        return Files.lines(Path.of(filePath)).toList();
+    }
+    
+    public static HashMap<String, Integer> processLines(List<String> lines) throws IOException {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String line : lines) {
+            if (line.length() == 0) continue;
+//            if (!line.contains("$")) System.out.println("Error!");
+            if (line.indexOf("$") == -1) System.out.println("Error!");
+            else {
+                String [] lineSplit = line.split("\\$");
+                String key = lineSplit[0].strip();
+                int value = lineSplit[1].strip().split(" ").length;
+                map.put(key, value);
+            }
+        }
+        return map;
+    }
+    
+    public static void writeLines (String filePath, HashMap<String, Integer> map) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (Map.Entry<String, Integer> entry : map.entrySet()){
+            bw.write(entry.getKey().toUpperCase() + " - " + entry.getValue()  +"\n");
+        }
+        bw.close();
+    }
+    
 }
